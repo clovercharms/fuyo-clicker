f61b8818c0993bd6db2f6cfb72f05c80b30e42e3
frammie <fraym.phone@gmail.com>
2023-11-04 00:19:55 +0100

    Update astronaut helmet

--- src/assets/images/clover/base/hero/index.ts
+++ src/assets/images/clover/base/hero/index.ts
@@ -1,5 +1,6 @@
 import hero from "./hero.png";
+import hazmat from "./hazmat.png";
 import alien from "./alien.png";
 import sketch from "./sketch.png";
 
-export { hero, alien, sketch };
+export { hero, hazmat, alien, sketch };

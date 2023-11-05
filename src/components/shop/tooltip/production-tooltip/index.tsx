f7a9860c49139ab4c46d983f0eae185704f3e3fa
frammie <fraym.phone@gmail.com>
2023-11-05 19:58:10 +0100

    Derive clicker prod. in tooltip

--- src/components/shop/tooltip/production-tooltip/index.tsx
+++ src/components/shop/tooltip/production-tooltip/index.tsx
@@ -28,7 +28,7 @@ export const ProductionTooltip = memo(
         const upgrades = countUnlockedUpgrades(unlockedUpgrades);
         const isClicker = items[itemId].laneType === undefined;
 
-        // Calculate total production
+        // Calculate total production.
         let productionSec = 0;
         if (isClicker) {
             productionSec =
@@ -45,15 +45,17 @@ export const ProductionTooltip = memo(
         // Calculate single unit production.
         let singleProductionSec = 0;
         if (isClicker) {
-            singleProductionSec = CLICKER_RATE_MS * 1e3;
+            if (coins.clickers === 0) {
+                singleProductionSec = CLICKER_RATE_MS * 1e3;
+            } else {
+                singleProductionSec = productionSec / coins.clickers;
+            }
         } else {
-            // Check unlocked, show base rate.
             const buildings = lanes[items[itemId].laneType!].buildings;
             if (buildings === 0) {
                 singleProductionSec =
                     lanesData[items[itemId].laneType!].rateMs * 1e3;
             } else {
-                // Otherwise derive from total production.
                 singleProductionSec = productionSec / buildings;
             }
         }

2d8eade01b6b6ad543207eb7cc30036bc2af8016
frammie <fraym.phone@gmail.com>
2023-11-09 21:53:24 +0100

    Fix loss of sfx interval due to effect render

--- src/hooks/useSoundEmitter.ts
+++ src/hooks/useSoundEmitter.ts
@@ -14,6 +14,11 @@ export interface SoundEmitterProps {
     enabled: boolean;
 }
 
+interface Scheduled {
+    started: number;
+    duration: number;
+}
+
 export function useSoundEmitter({
     sounds,
     intervalRangeMs,
@@ -22,22 +27,33 @@ export function useSoundEmitter({
     const play = useSettingsStore(settings => settings.audio.play);
 
     const timeoutId = useRef<number | undefined>();
-    const [scheduled, setScheduled] = useState(false);
+    const [scheduled, setScheduled] = useState<Scheduled | null>(null);
 
     const playSound = useCallback(() => {
         void play(sounds[dist(0, sounds.length - 1, RNG)]).catch();
-        setScheduled(false);
+        setScheduled(null);
     }, [play, sounds]);
 
     useEffect(() => {
         window.clearTimeout(timeoutId.current);
         timeoutId.current = undefined;
-        if (!enabled) return;
+        if (!enabled || sounds.length === 0) return;
 
-        const timeout = dist(intervalRangeMs[0], intervalRangeMs[1], RNG);
+        // Schedule new
+        let current = scheduled;
+        const time = performance.now();
+        if (!current) {
+            current = {
+                started: time,
+                duration: dist(intervalRangeMs[0], intervalRangeMs[1], RNG),
+            };
+            setScheduled(current);
+        }
+
+        const elapsed = time - current.started;
+        const timeout = current.duration - elapsed;
         timeoutId.current = window.setTimeout(playSound, timeout);
-        setScheduled(true);
 
         return () => window.clearTimeout(timeoutId.current);
-    }, [enabled, intervalRangeMs, playSound, scheduled]);
+    }, [sounds, enabled, intervalRangeMs, playSound, scheduled]);
 }

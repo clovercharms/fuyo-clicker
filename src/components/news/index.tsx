df6f82c03d24839bc3c32975a7107b4503a23281
LuckyCharmDani <waifudispenser@gmail.com>
2023-11-12 13:18:29 +0000

    Created basic news reel without stylizing

--- src/components/news/index.tsx
+++ src/components/news/index.tsx
@@ -1,20 +1,28 @@
 import cx from "classix";
 import { useGameStore } from "@/stores/game";
 import classes from "./index.module.css";
-import { HTMLProps } from "react";
+import { HTMLProps, useEffect, useState } from "react";
 import { SoundType } from "@/utils/audio/sounds";
 import { Button } from "../button";
 import { AudioToggle } from "./audio-toggle";
-
+import { newsreel } from "@/assets/json_data/data";
 /**
  * Newsreel that displays news based on current or random events.
  */
 export default function News(props: HTMLProps<HTMLDivElement>) {
     const reset = useGameStore(state => state.reset);
+    const [indexNews, setIndexNews] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setIndexNews(indexNews => indexNews + 1)
+        }, 10000);
+        return () => clearInterval(interval);
+      }, []);
 
     return (
         <div {...props} className={cx(classes.news, props.className)}>
-            <h1>News</h1>
+            <div>{newsreel[indexNews].content}</div>
+            <div> - {newsreel[indexNews].author}</div>
             <div className={classes.controls}>
                 <Button
                     onClick={() => {
@@ -22,7 +30,8 @@ export default function News(props: HTMLProps<HTMLDivElement>) {
                         window.location.reload();
                     }}
                 >
-                    Reset all progress
+                    Reset all progress 
+                    
                 </Button>
                 <AudioToggle type={SoundType.Music} label="Music" />
                 <AudioToggle type={SoundType.SFX} label="SFX" />

f73ec0c91ea12ac6a2154287c7469dbf07a8b859
frammie <fraym.phone@gmail.com>
2023-11-19 20:20:56 +0100

    Remove spoiler and codename

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { Spoiler } from "@/components/spoiler/index.tsx";
 
 import { FuyoClicker } from "./FuyoClicker.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Spoiler>
-            <FuyoClicker />
-        </Spoiler>
+        <FuyoClicker />
     </StrictMode>
 );
